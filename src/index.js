2906d9f1b3248820108ef9cdd6d4b08281bc23a0
James Hagood <cardfaux@tutanota.com>
2020-02-22 14:04:46 -0600

    added styled-components themeProvider

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ThemeProvider } from 'styled-components';
 
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const theme = {
+	colors: {
+		primary: '#89cff0',
+		secondary: '#f089cf'
+	}
+};
+
+ReactDOM.render(
+	<ThemeProvider theme={theme}>
+		<App />
+	</ThemeProvider>,
+	document.getElementById('root')
+);
