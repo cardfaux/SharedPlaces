21a2235a27ae85291c183022044e3048e094ef38
James Hagood <cardfaux@tutanota.com>
2020-02-25 15:45:10 -0600

    fixed a cpl bugs

--- src/places/pages/UpdatePlace.js
+++ src/places/pages/UpdatePlace.js
@@ -79,7 +79,8 @@ const UpdatePlace = ({ className }) => {
 					description: formState.inputs.description.value
 				}),
 				{
-					'Content-Type': 'application/json'
+					'Content-Type': 'application/json',
+					Authorization: 'Bearer ' + auth.token
 				}
 			);
 			history.push('/' + auth.userId + '/places');
